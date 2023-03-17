ed0ed9e0a7bfaf6527cbc4cdb013cd50112e5096
Krystian Kluba <klubakrystian@gmail.com>
2023-03-17 18:56:17 +0100

    Added KlientTest

---
+++ nauka/programowanie_obiektowe/ABC/ABC.BLTest/KlientTest.cs
@@ -0,0 +1,25 @@
+﻿using ABC.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ABC.BLTest
+{
+    [TestClass]
+    public class KlientTest
+    {
+        [TestMethod]
+        public void ImieNazwiskoTest()
+        {
+            // Arrange
+            Klient klient = new Klient();
+            klient.Imie = "Tomasz";
+            klient.Nazwisko = "Nowak";
+            string oczekiwana = "Tomasz, Nowak";
+
+            // Act
+            string aktualna = klient.ImieNazwisko;
+
+            // Assert
+            Assert.AreEqual(oczekiwana, aktualna);
+        }
+    }
+}
---
+++ nauka/programowanie_obiektowe/ABC/ABC.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ABC.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ABC.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dfead52e-6e6c-4829-86c7-a7d6a1410ca3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
