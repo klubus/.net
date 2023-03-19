f8a20b47a77447d1d55ff501f91b80410923ebaa
Krystian Kluba <klubakrystian@gmail.com>
2023-03-19 13:33:35 +0100

    Added class Zamowienie

---
+++ nauka/programowanie_obiektowe/ABC/ABC.BL/Zamowienie.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ABC.BL
+{
+    public class Zamowienie
+    {
+        public Zamowienie()
+        {
+
+        }
+
+        public Zamowienie(int zamowienieId)
+        {
+            ZamowienieId = zamowienieId;
+        }
+
+        public int ZamowienieId { get; private set; }
+
+        // DateTimeOffset wspomaga współpracę z różnymi strefami czasowymi
+        public DateTimeOffset? DataZamowienia { get; set; }
+
+        /// <summary>
+        /// Pobieramy jedno zamowienie
+        /// </summary>
+        /// <param name="produktId"></param>
+        /// <returns></returns>
+        public Zamowienie Pobierz(int zamowienieId)
+        {
+            // kod, który pobiera zdefiniowane zamowienie
+            return new Zamowienie();
+        }
+
+        /// <summary>
+        /// Zapisujemy bieżące zamówienie
+        /// </summary>
+        /// <param name="produktId"></param>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            // kod, który zapisuje zdefiniowane zamówienie
+            return true;
+        }
+
+        /// <summary>
+        /// Sprawdzamy dane zamówienia
+        /// </summary>
+        /// <returns></returns>
+        public bool Zwaliduj()
+        {
+            var poprawne = true;
+
+            if (DataZamowienia == null)
+                poprawne = false;
+
+            return poprawne;
+        }
+
+    }
+}
