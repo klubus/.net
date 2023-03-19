edd5b27404537124546b381986db42b308c29acf
Krystian Kluba <klubakrystian@gmail.com>
2023-03-19 08:49:02 +0100

    Added class Produkt

--- nauka/programowanie_obiektowe/ABC/ABC.BL/Produkt.cs
+++ nauka/programowanie_obiektowe/ABC/ABC.BL/Produkt.cs
@@ -1,12 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ABC.BL
 {
-    internal class Produkt
+    public class Produkt
     {
+        public Produkt()
+        {
+
+        }
+
+        public Produkt(int produktId)
+        {
+            ProduktId = produktId;
+        }
+
+        public int ProduktId { get; private set; }
+        public Decimal? AktualnaCena { get; set; }
+        public string Opis { get; set; }
+        public string NazwaProduktu { get; set; }
+
+        /// <summary>
+        /// Pobieramy jeden produkt
+        /// </summary>
+        /// <param name="produktId"></param>
+        /// <returns></returns>
+        public Produkt Pobierz(int produktId)
+        {
+            // kod, który pobiera zdefiniowany produkt
+            return new Produkt();
+        }
+
+        /// <summary>
+        /// Zapisujemy bieżący produkt
+        /// </summary>
+        /// <param name="produktId"></param>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            // kod, który zapisuje zdefiniowany produkt
+            return true;
+        }
+
+        /// <summary>
+        /// Sprawdzamy dane produktu
+        /// </summary>
+        /// <returns></returns>
+        public bool Zwaliduj()
+        {
+            var poprawne = true;
+
+            if (string.IsNullOrWhiteSpace(NazwaProduktu))
+                poprawne = false;
+
+            return poprawne;
+        }
+
     }
 }
