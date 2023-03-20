fab5631fb0c1004408ad3be5a2acdfb03b214458
Krystian Kluba <klubakrystian@gmail.com>
2023-03-20 17:01:31 +0100

    Added class KlientRepository

---
+++ nauka/programowanie_obiektowe/ABC/ABC.BL/KlientRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace ABC.BL
+{
+    public class KlientRepository
+    {
+        /// <summary>
+        /// Zapisujemy obecnego klienta
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        public bool Zapisz()
+        {
+            // kod, który zapisuje zdefiniowanego klienta
+            return true;
+        }
+
+
+        /// <summary>
+        /// Pobieramy jednego klienta
+        /// </summary>
+        /// <param name="klientId"></param>
+        /// <returns></returns>
+        public Klient Pobierz(int klientId)
+        {
+            //Tworzymy instancję klasy klienta
+            Klient klient = new Klient(klientId);
+
+            // kod, który pobiera określonego klienta
+
+            // Tymczasowo zakodowane wartości, aby zwrócić klienta
+            if (klientId == 1)
+            {
+                klient.Email = "marcin@dev-hobyy.pl";
+                klient.Imie = "Marcin";
+                klient.Nazwisko = "Nowak";
+            }
+
+            return new Klient();
+        }
+
+
+        /// <summary>
+        /// Pobieramy wszystkich klientów
+        /// </summary>
+        /// <returns></returns>
+        public List<Klient> Pobierz()
+        {
+            // kod, który pobiera wszystkich klientów
+            return new List<Klient>();
+        }
+    }
+}
