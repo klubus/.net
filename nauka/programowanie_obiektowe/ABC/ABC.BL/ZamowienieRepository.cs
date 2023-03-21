240a524abf7f658346b8fd36febcd370c16c090e
Krystian Kluba <klubakrystian@gmail.com>
2023-03-21 16:51:40 +0100

    Added class ZamowienieRepository

---
+++ nauka/programowanie_obiektowe/ABC/ABC.BL/ZamowienieRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ABC.BL
+{
+    public class ZamowienieRepository
+    {
+
+        /// <summary>
+        /// Pobieramy jedno zamowienie
+        /// </summary>
+        /// <param name="produktId"></param>
+        /// <returns></returns>
+        public Zamowienie Pobierz(int zamowienieId)
+        {
+            // Tworzymy instancję klasy zamówienia i przekazujemy identyfikator
+            Zamowienie zamowienie = new Zamowienie(zamowienieId);
+
+            // kod, który pobiera zdefiniowane zamowienie
+
+            // Tymczasowo zakodowane wartości zamówienia do zwrócenia
+            if (zamowienieId == 10)
+            {
+                zamowienie.DataZamowienia = new DateTimeOffset(2018, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0));
+            }
+            return zamowienie;
+        }
+
+
+        /// <summary>
+        /// Zapisujemy bieżące zamówienie
+        /// </summary>
+        /// <param name="produktId"></param>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            // kod, który zapisuje zdefiniowane zamówienie
+            return true;
+        }
+    }
+}
