1ef57284713795d29b5ad321dbd6e1e0cd6561e3
Krystian Kluba <klubakrystian@gmail.com>
2023-03-21 16:02:45 +0100

    Added class ProduktRepository

---
+++ nauka/programowanie_obiektowe/ABC/ABC.BL/ProduktRepository.cs
@@ -0,0 +1,41 @@
+﻿namespace ABC.BL
+{
+    public class ProduktRepository
+    {
+        /// <summary>
+        /// Pobieramy jeden produkt
+        /// </summary>
+        /// <param name="produktId"></param>
+        /// <returns></returns>
+        public Produkt Pobierz(int produktId)
+        {
+            // Tworzymy instancję klasy Produkt i przekazujemy identyfikator
+            Produkt produkt = new Produkt(produktId);
+
+            // kod, który pobiera zdefiniowany produkt
+
+            // Tymczasowe zakodowane wartości, aby zwrócić produkt
+            if (produktId == 2)
+            {
+                produkt.NazwaProduktu = "Klocki";
+                produkt.Opis = "Klocki do zabawy dla dzieci powyżej 3 lat";
+                produkt.AktualnaCena = 199.99M;
+
+            }
+
+            return produkt;
+        }
+
+
+        /// <summary>
+        /// Zapisujemy bieżący produkt
+        /// </summary>
+        /// <param name="produktId"></param>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            // kod, który zapisuje zdefiniowany produkt
+            return true;
+        }
+    }
+}
