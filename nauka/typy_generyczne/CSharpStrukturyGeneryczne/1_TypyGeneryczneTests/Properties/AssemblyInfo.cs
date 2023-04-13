3fc96d21240082054a988c6f685088e5f18cd7a6
Krystian Kluba <klubakrystian@gmail.com>
2023-04-13 17:42:22 +0200

    Added tests to KolejkaKolowa

---
+++ nauka/typy_generyczne/CSharpStrukturyGeneryczne/1_TypyGeneryczneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("1_TypyGeneryczneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1_TypyGeneryczneTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("612fe436-cdd7-4dc6-9e87-a1882973d0e2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
