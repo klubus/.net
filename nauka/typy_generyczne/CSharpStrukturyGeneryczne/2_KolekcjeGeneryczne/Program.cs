aedda5ac232a81f6eb3f1aef759d0378bb61c601
Krystian Kluba <klubakrystian@gmail.com>
2023-04-18 16:23:51 +0200

    Stos

--- nauka/typy_generyczne/CSharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
+++ nauka/typy_generyczne/CSharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
@@ -13,11 +13,27 @@ namespace _2_KolekcjeGeneryczne
             kolejka.Enqueue(new Pracownik { Imie = "Jacek", Nazwisko = "Zajac" });
             kolejka.Enqueue(new Pracownik { Imie = "Ola", Nazwisko = "Kaczor" });
 
+            Console.WriteLine("Kolejka");
             while (kolejka.Count > 0)
             {
                 var pracownik = kolejka.Dequeue();
                 Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
             }
+
+            Stack<Pracownik> stos = new Stack<Pracownik>();
+            stos.Push(new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
+            stos.Push(new Pracownik { Imie = "Tomek", Nazwisko = "Nowak" });
+            stos.Push(new Pracownik { Imie = "Jacek", Nazwisko = "Zajac" });
+            stos.Push(new Pracownik { Imie = "Ola", Nazwisko = "Kaczor" });
+
+            Console.WriteLine();
+            Console.WriteLine("Stos");
+            while (stos.Count > 0)
+            {
+                var pracownik = stos.Pop();
+                Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
+            }
+
             Console.ReadKey();
         }
     }
