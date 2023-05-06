9a46ef24d9522ed45834966b3a149bbdf3d7ab47
Krystian Kluba <klubakrystian@gmail.com>
2023-05-06 09:33:58 +0200

    Added new project - Funkcje

---
+++ nauka/linq/LINQ/2_Funkcje/Pracownik.cs
@@ -0,0 +1,10 @@
+﻿namespace _2_Funkcje
+{
+    public class Pracownik
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+    }
+}
