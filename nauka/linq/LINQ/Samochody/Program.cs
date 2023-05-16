ec14b9d20e3ac06fd02190933e0e40834a0d45f9
Krystian Kluba <klubakrystian@gmail.com>
2023-05-16 18:19:36 +0200

    File loading - query syntax

--- nauka/linq/LINQ/Samochody/Program.cs
+++ nauka/linq/LINQ/Samochody/Program.cs
@@ -9,13 +9,21 @@ namespace Samochody
     {
         static void Main(string[] args)
         {
-            var samochody = wczytywaniePliku("C:/Users/kluba/Pulpit/dotnet/.net/nauka/linq/LINQ/Samochody/paliwo.csv");
+            var samochody = wczytywaniePliku2("C:/Users/kluba/Pulpit/dotnet/.net/nauka/linq/LINQ/Samochody/paliwo.csv");
 
             foreach (var item in samochody)
             {
                 Console.WriteLine(item.Producent + " " + item.Model);
             }
             Console.ReadLine();
+        }
+
+        private static List<Samochod> wczytywaniePliku2(string sciezka)
+        {
+            var zapytanie = (from linia in File.ReadAllLines(sciezka).Skip(1)
+                             where linia.Length > 1
+                             select Samochod.ParsujCSV(linia));
+            return zapytanie.ToList();
         }
 
         private static List<Samochod> wczytywaniePliku(string sciezka)
