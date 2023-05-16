930285dbf69d6a8bfc4114ed249db012f26f9252
Krystian Kluba <klubakrystian@gmail.com>
2023-05-16 18:12:33 +0200

    File loading

--- nauka/linq/LINQ/Samochody/Program.cs
+++ nauka/linq/LINQ/Samochody/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace Samochody
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Samochody
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            var samochody = wczytywaniePliku("C:/Users/kluba/Pulpit/dotnet/.net/nauka/linq/LINQ/Samochody/paliwo.csv");
+
+            foreach (var item in samochody)
+            {
+                Console.WriteLine(item.Producent + " " + item.Model);
+            }
+            Console.ReadLine();
         }
+
+        private static List<Samochod> wczytywaniePliku(string sciezka)
+        {
+            return File.ReadAllLines(sciezka)
+                       .Skip(1)
+                       .Where(linia => linia.Length > 1)
+                       .Select(Samochod.ParsujCSV).ToList();
+        }
+
     }
 }
