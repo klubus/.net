5f80a630be5eab8f1e857f6a48d8ac45895f9081
Krystian Kluba <klubakrystian@gmail.com>
2023-05-17 18:27:16 +0200

    Filtering

--- nauka/linq/LINQ/Samochody/Program.cs
+++ nauka/linq/LINQ/Samochody/Program.cs
@@ -11,12 +11,20 @@ namespace Samochody
         {
             var samochody = wczytywaniePliku2("C:/Users/kluba/Pulpit/dotnet/.net/nauka/linq/LINQ/Samochody/paliwo.csv");
 
-            var zapytanie = samochody.OrderByDescending(s => s.SpalanieAutostrada).ThenBy(s => s.Producent);
+            var zapytanie = samochody.OrderByDescending(s => s.SpalanieAutostrada)
+                                     .ThenBy(s => s.Producent)
+                                     .Select(s => s)
+                                     .FirstOrDefault(s => s.Producent == "ccc" && s.Rok == 2018);
 
             var zapytanie2 = from samochod in samochody
+                             where samochod.Producent == "Audi" && samochod.Rok == 2018
                              orderby samochod.SpalanieAutostrada descending, samochod.Producent ascending
                              select samochod;
 
+            if (zapytanie != null)
+            {
+                Console.WriteLine(zapytanie.Producent + " " + zapytanie.Model);
+            }
 
             foreach (var item in zapytanie2.Take(10))
             {
