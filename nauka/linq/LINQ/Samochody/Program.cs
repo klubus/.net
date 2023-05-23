4d0a186f89e0fc7c935bc8e70f396e9eba3cd267
Krystian Kluba <klubakrystian@gmail.com>
2023-05-23 16:51:23 +0200

    Join query method

--- nauka/linq/LINQ/Samochody/Program.cs
+++ nauka/linq/LINQ/Samochody/Program.cs
@@ -13,19 +13,20 @@ namespace Samochody
             var producenci = WczytywanieProducenci("C:/Users/kluba/Pulpit/dotnet/.net/nauka/linq/LINQ/Samochody/producent.csv");
 
             var zapytanie = from samochod in samochody
-                            where samochod.Producent == "Audi" && samochod.Rok == 2018
+                            join producent in producenci on samochod.Producent equals producent.Nazwa
                             orderby samochod.SpalanieAutostrada descending, samochod.Producent ascending
                             select new
                             {
                                 samochod.Producent,
                                 samochod.Model,
+                                producent.Siedziba,
                                 samochod.SpalanieAutostrada
                             };
 
 
             foreach (var samochod in zapytanie.Take(10))
             {
-                Console.WriteLine(samochod.Producent + " " + samochod.Model + " " + samochod.SpalanieAutostrada);
+                Console.WriteLine(samochod.Siedziba + " " + samochod.Producent + " " + samochod.Model + " " + samochod.SpalanieAutostrada);
             }
             Console.ReadLine();
         }
