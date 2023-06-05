740f4f2589bb8e23a259d7af822cd026450c17d4
Krystian Kluba <klubakrystian@gmail.com>
2023-06-05 12:32:07 +0200

    Added User model

---
+++ projekty/Evento/src/Evento.Core/Domain/Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Users : Entity
+{
+    public string Role { get; protected set; }
+    public string Name { get; protected set; }
+    public string Email { get; protected set; }
+    public string Password { get; protected set; }
+    public DateTime CreatedAt { get; protected set; }
+
+    
+
+    public Users()
+	{
+	}
+
+    protected Users(Guid id, string role, string name, string email, string password)
+    {
+        Id = id;
+        Role = role;
+        Name = name;
+        Email = email;
+        Password = password;
+        CreatedAt = DateTime.Now;
+    }
+}
