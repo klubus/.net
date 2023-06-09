08873ae9443a37f1a8dd3b2da075be4ca5a60474
Krystian Kluba <klubakrystian@gmail.com>
2023-06-09 16:04:08 +0200

    Implement UserRepository

--- projekty/Evento/src/Evento.Infrastructure/Repositories/UserRepository.cs
+++ projekty/Evento/src/Evento.Infrastructure/Repositories/UserRepository.cs
@@ -5,34 +5,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Evento.Infrastructure.Repositories
 {
     public class UserRepository : IUserRepository
     {
-        public Task AddAsync(User user)
+        private static readonly ISet<User> _users = new HashSet<User>();
+
+        public async Task<User> GetAsync(Guid id)
+            => await Task.FromResult(_users.SingleOrDefault(x => x.Id == id));
+
+        public async Task<User> GetAsync(string email)
+               => await Task.FromResult(_users.SingleOrDefault(x => x.Email.ToLowerInvariant() == email.ToLowerInvariant()));
+
+        public async Task AddAsync(User user)
         {
-            throw new NotImplementedException();
+            _users.Add(user);
+            await Task.CompletedTask;
         }
 
-        public Task DeleteAsync(User user)
+        public async Task UpdateAsync(User user)
         {
-            throw new NotImplementedException();
+            await Task.CompletedTask;
         }
 
-        public Task<User> GetAsync(Guid id)
+        public async Task DeleteAsync(User user)
         {
-            throw new NotImplementedException();
+            _users.Add(user);
+            await Task.CompletedTask;
         }
 
-        public Task<User> GetAsync(string email)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateAsync(User user)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
