d9d0452768be32ea3d8f867d92024aa3708dd86f
Krystian Kluba <klubakrystian@gmail.com>
2023-06-09 20:05:29 +0200

    Added Events Controller

--- projekty/Evento/src/Evento.Api/Program.cs
+++ projekty/Evento/src/Evento.Api/Program.cs
@@ -1,3 +1,8 @@
+using Evento.Core.Repositories;
+using Evento.Infrastructure.Mappers;
+using Evento.Infrastructure.Repositories;
+using Evento.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddSingleton(AutoMapperConfig.Initialize());
 
 var app = builder.Build();
 
