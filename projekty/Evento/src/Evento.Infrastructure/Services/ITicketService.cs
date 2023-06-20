510f999b9689755dad749364d9e16c1139daed4b
Krystian Kluba <k.kluba@mccomp.pl>
2023-06-20 17:57:08 +0200

    Added Ticket Service

---
+++ projekty/Evento/src/Evento.Infrastructure/Services/ITicketService.cs
@@ -0,0 +1,16 @@
+﻿using Evento.Infrastructure.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evento.Infrastructure.Services
+{
+    public interface ITicketService
+    {
+        Task<TicketDto> GetAsync(Guid userId, Guid eventId, Guid ticketId);
+        Task PurchaseAsync(Guid userId, Guid eventId, int amount);
+        Task CancelAsync(Guid userId, Guid eventId, int amount);
+    }
+}
