4eedd7cb38bf02fadae94c1b7b4ac7bd1f539f4e
Krystian Kluba <klubakrystian@gmail.com>
2023-09-19 20:34:02 +0200

    Added GetRegionAsync controller

--- projekty/NZWalks/NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ projekty/NZWalks/NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -20,7 +20,7 @@ namespace NZWalks.API.Controllers
         public IMapper Mapper { get; }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllRegions()
+        public async Task<IActionResult> GetAllRegionsAsync()
         {
             var regions = await regionRepository.GetAllAsync();
 
@@ -44,6 +44,21 @@ namespace NZWalks.API.Controllers
             var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
 
             return Ok(regionsDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetRegionAsync(Guid id)
+        {
+            var region = await regionRepository.GetAsync(id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            var regionDTO = mapper.Map<Models.DTO.Region>(region);
+            return Ok(regionDTO);
         }
     }
 }
