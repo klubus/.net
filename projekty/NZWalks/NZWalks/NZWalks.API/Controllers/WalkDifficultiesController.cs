49b9601a69a5e39a4e2177059b633d613a00677a
Krystian Kluba <klubakrystian@gmail.com>
2023-09-27 14:19:54 +0200

    Added GetAllWalksDifficultiesAsync

---
+++ projekty/NZWalks/NZWalks/NZWalks.API/Controllers/WalkDifficultiesController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalkDifficultiesController : Controller
+    {
+        private readonly IWalkDifficultyRepository walkDifficultyRepository;
+        private readonly IMapper mapper;
+
+        public WalkDifficultiesController(IWalkDifficultyRepository walkDifficultyRepository, IMapper mapper)
+        {
+            this.walkDifficultyRepository = walkDifficultyRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWalksDifficultyAsync()
+        {
+            var walksDifficultyDomain = await walkDifficultyRepository.GetAllAsync();
+
+            var walksDifficultyDTO = mapper.Map<List<Models.DTO.WalkDifficulty>>(walksDifficultyDomain);
+            return Ok(walksDifficultyDTO);
+
+        }
+    }
+}
---
+++ projekty/NZWalks/NZWalks/NZWalks.API/Repositories/WalkDifficultyRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class WalkDifficultyRepository : IWalkDifficultyRepository
+    {
+        private readonly NZWalksDbContext nZWalksDbContext;
+
+        public WalkDifficultyRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+        public async Task<IEnumerable<WalkDifficulty>> GetAllAsync()
+        {
+            return await nZWalksDbContext.WalkDifficulty.ToListAsync();
+        }
+    }
+}
