a34ea0bc0a8e57129643ca837771f0da714273e0
Krystian Kluba <klubakrystian@gmail.com>
2023-10-11 20:17:43 +0200

    Added AuthCotroller

--- projekty/NZWalks/NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ projekty/NZWalks/NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Repositories;
 
@@ -63,6 +64,7 @@ namespace NZWalks.API.Controllers
         }
 
         [HttpPost]
+        [Authorize]
         public async Task<IActionResult> AddRegionAsync(Models.DTO.AddRegionRequest addRegionRequest)
         {
             // Validae The Request
@@ -102,6 +104,7 @@ namespace NZWalks.API.Controllers
 
         [HttpDelete]
         [Route("{id:guid}")]
+        [Authorize]
         public async Task<IActionResult> DeleteRegionAsync(Guid id)
         {
             // Get region from database
@@ -131,6 +134,7 @@ namespace NZWalks.API.Controllers
 
         [HttpPut]
         [Route("{id:guid}")]
+        [Authorize]
         public async Task<IActionResult> UpdateRegionAsync([FromRoute] Guid id, [FromBody] Models.DTO.UpdateRegionRequest updateRegionRequest)
         {
             // Validate the incoming request
--- projekty/NZWalks/NZWalks/NZWalks.API/Program.cs
+++ projekty/NZWalks/NZWalks/NZWalks.API/Program.cs
@@ -25,6 +25,11 @@ builder.Services.AddDbContext<NZWalksDbContext>(options =>
 builder.Services.AddScoped<IRegionRepository, RegionRepository>();
 builder.Services.AddScoped<IWalkRepository, WalkRepository>();
 builder.Services.AddScoped<IWalkDifficultyRepository, WalkDifficultyRepository>();
+builder.Services.AddSingleton<IUserRepository, StaticUserRepository>();
+builder.Services.AddScoped<ITokenHandler, NZWalks.API.Repositories.TokenHandler>();
+
+
+
 
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
