61a3842975d2757f24e2d9e9ad621d61a0ee6bda
Krystian Kluba <klubakrystian@gmail.com>
2023-10-22 12:47:01 +0200

    Basic Producer

--- nauka/rabbitmq/ProducerConsumerRabbitMQ/Producer/Sender.cs
+++ nauka/rabbitmq/ProducerConsumerRabbitMQ/Producer/Sender.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("BasicTest", false, false, false, null);
+
+    string message = "Getting started with .Net Core RabbitMQ";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "BasicTest", null, body);
+    Console.WriteLine("Sent message {0}...", message);
+}
+
+Console.WriteLine("Press [enter] to exit the Sender App");
+Console.ReadLine();
