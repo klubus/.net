1df1f829ecaa472ee762c4f25645c62e20077b2d
Krystian Kluba <klubakrystian@gmail.com>
2023-11-01 13:58:50 +0100

    Init Migration Transfer

--- nauka/rabbitmq/MicroRabbit/MicroRabbit.Transfer.Api/Program.cs
+++ nauka/rabbitmq/MicroRabbit/MicroRabbit.Transfer.Api/Program.cs
@@ -1,3 +1,10 @@
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +12,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TransferDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TransferDbConnecion"));
+});
+
+builder.Services.AddTransient<IEventBus, RabbitMQBus>();
+/*builder.Services.AddTransient<IAccountService, AccountService>();
+builder.Services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
+builder.Services.AddTransient<IAccountRepository, AccountRepository>();
+builder.Services.AddTransient<BankingDbContext>();*/
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+
+builder.Services.AddSwaggerGen(options =>
+{
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "JWT Authentication",
+        Description = "Enter a valid JWT bearer token",
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+    };
+});
 
 var app = builder.Build();
 
