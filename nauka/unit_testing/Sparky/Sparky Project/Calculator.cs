a1d7a275488bf725bf0f4553e81dca06db2853d8
Krystian Kluba <klubakrystian@gmail.com>
2023-11-05 19:25:49 +0100

    Added Unit Test projects and first test

---
+++ nauka/unit_testing/Sparky/Sparky Project/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Sparky_Project
+{
+    public class Calculator
+    {
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ nauka/unit_testing/Sparky/SparkyMSTest/CalculatorMSTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sparky_Project;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+
+            //Act
+            int result = calculator.AddNumbers(10, 20);
+
+            //Assert
+            Assert.AreEqual(30, result);
+        }
+    }
+}
