694cd1ba57d312134cfc85142fe53c3094f313da
Krystian Kluba <klubakrystian@gmail.com>
2023-11-09 21:48:43 +0100

    Added Sparky NUnit Test project

---
+++ nauka/unit_testing/Sparky/Sparky NUnit Test/CalculatorNUnitTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using Sparky_Project;
+
+namespace Sparky_NUnit_Test
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        [Test]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+
+            //Act
+            int result = calculator.AddNumbers(10, 20);
+
+            //Assert
+            Assert.AreEqual(30, result);
+        }
+
+        [Test]
+        public void IsOddChecker_InputEvenNumber_ReturnFalse()
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+
+            //Act
+            int number = calculator.AddNumbers(10, 20);
+            bool result = calculator.IsOddNumber(number);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        [TestCase(11)]
+        [TestCase(13)]
+        public void IsOddChecker_InputOddNumber_ReturnTrue(int a)
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+
+            //Act
+            int number = calculator.AddNumbers(a, 20);
+            bool result = calculator.IsOddNumber(number);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        [TestCase(10, ExpectedResult = false)]
+        [TestCase(11, ExpectedResult = true)]
+        public bool IsOddChecker_InputNumber_ReturnTrueIfOdd(int a)
+        {
+            Calculator calculator = new Calculator();
+            return calculator.IsOddNumber(a);
+        }
+
+        [Test]
+        [TestCase(5.4, 10.5)] // 15.9
+        [TestCase(5.43, 10.53)] // 15.96
+        [TestCase(5.49, 10.59)] // 16.08
+        public void AddNumbersDouble_InputTwoDouble_GetCorrectAddition(double a, double b)
+        {
+            //Arrange
+            Calculator calculator = new();
+
+            //Act
+            double result = calculator.AddNumbersDouble(a, b);
+
+            //Assert
+            Assert.AreEqual(15.9, result, .2);
+
+        }
+    }
+}
