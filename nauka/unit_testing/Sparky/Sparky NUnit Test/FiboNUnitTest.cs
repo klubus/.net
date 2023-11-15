0e297259d158d3d4adacee0b3b8159ad961b6d4b
Krystian Kluba <klubakrystian@gmail.com>
2023-11-15 20:12:23 +0100

    Assignment 3

---
+++ nauka/unit_testing/Sparky/Sparky NUnit Test/FiboNUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Sparky_Project;
+
+namespace Sparky_NUnit_Test
+{
+    [TestFixture]
+    public class FiboNUnitTest
+    {
+        private Fibo fibo;
+        [SetUp]
+        public void SetUp()
+        {
+            fibo = new Fibo();
+        }
+        [Test]
+        public void GetFiboSeries_Input1_ResultMultiple()
+        {
+            fibo.Range = 1;
+            var result = fibo.GetFiboSeries();
+
+            Assert.That(result, Is.Not.Empty);
+            Assert.That(result, Is.Ordered);
+            Assert.That(result, Is.EqualTo(new List<int> { 0 }));
+        }
+
+        [Test]
+        public void GetFiboSeries_Input6_ResultMultiple()
+        {
+            fibo.Range = 6;
+            var result = fibo.GetFiboSeries();
+
+            Assert.That(result, Does.Contain(3));
+            Assert.That(result.Count, Is.EqualTo(6));
+            Assert.That(result, Does.Not.Contain(4));
+            Assert.That(result, Is.EqualTo(new List<int> { 0, 1, 1, 2, 3, 5 }));
+        }
+    }
+}
