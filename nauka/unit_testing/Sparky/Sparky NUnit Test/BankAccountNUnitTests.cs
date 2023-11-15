c1e11c11044aa7b3951f0d606705dc0c76bb7e15
Krystian Kluba <klubakrystian@gmail.com>
2023-11-15 21:29:13 +0100

    Using MOQ

---
+++ nauka/unit_testing/Sparky/Sparky NUnit Test/BankAccountNUnitTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using NUnit.Framework;
+using Sparky_Project;
+
+namespace Sparky_NUnit_Test
+{
+    [TestFixture]
+    public class BankAccountNUnitTests
+    {
+        private BankAccount account;
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void BankDepositLogFakker_Add100_ReturnTrue()
+        {
+            BankAccount bankAccount = new(new LogFakker());
+            var result = bankAccount.Deposit(100);
+            Assert.IsTrue(result);
+            Assert.That(bankAccount.GetBalance(), Is.EqualTo(100));
+        }
+
+        [Test]
+        public void BankDeposit_Add100_ReturnTrue()
+        {
+            var logMock = new Mock<ILogBook>();
+
+            BankAccount bankAccount = new(logMock.Object);
+            var result = bankAccount.Deposit(100);
+            Assert.IsTrue(result);
+            Assert.That(bankAccount.GetBalance(), Is.EqualTo(100));
+        }
+    }
+}
---
+++ nauka/unit_testing/Sparky/Sparky NUnit Test/ProductNUnitTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace Sparky_NUnit_Test
+{
+    [TestFixture]
+    public class ProductNUnitTests
+    {
+
+    }
+}
