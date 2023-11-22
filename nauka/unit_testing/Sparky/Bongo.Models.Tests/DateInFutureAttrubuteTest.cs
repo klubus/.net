35327ae1258e64c92298c060b24acc278c789c12
Krystian Kluba <klubakrystian@gmail.com>
2023-11-22 12:59:36 +0100

    Added DateInFutureAttributeTest

---
+++ nauka/unit_testing/Sparky/Bongo.Models.Tests/DateInFutureAttrubuteTest.cs
@@ -0,0 +1,26 @@
+﻿using Bongo.Models.ModelValidations;
+using NUnit.Framework;
+
+namespace Bongo.Models
+{
+    [TestFixture]
+    public class DateInFutureAttrubuteTest
+    {
+        [TestCase(100, ExpectedResult = true)]
+        [TestCase(0, ExpectedResult = false)]
+        [TestCase(-100, ExpectedResult = false)]
+        public bool DateValidator_InputExpectedDateRange_DateValidity(int addTime)
+        {
+            DateInFutureAttribute dateInFutureAttribute = new(() => DateTime.Now);
+            return dateInFutureAttribute.IsValid(DateTime.Now.AddHours(addTime));
+        }
+
+        [Test]
+        public void DateValidator_AnyDate_ReturnErrorMessage()
+        {
+            var result = new DateInFutureAttribute();
+            Assert.AreEqual("Date must be in the future", result.ErrorMessage);
+        }
+
+    }
+}
