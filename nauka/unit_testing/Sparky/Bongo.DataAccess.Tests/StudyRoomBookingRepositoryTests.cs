1eb093f503b3c6621146d55453d9b03c6e367467
Krystian Kluba <klubakrystian@gmail.com>
2023-11-22 23:08:17 +0100

    DataAccess Tests

---
+++ nauka/unit_testing/Sparky/Bongo.DataAccess.Tests/StudyRoomBookingRepositoryTests.cs
@@ -0,0 +1,116 @@
+﻿using Bongo.DataAccess.Repository;
+using Bongo.Models.Model;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Collections;
+
+namespace Bongo.DataAccess
+{
+    [TestFixture]
+    public class StudyRoomBookingRepositoryTests
+    {
+        private StudyRoomBooking studyRoomBooking_One;
+        private StudyRoomBooking studyRoomBooking_Two;
+        private DbContextOptions<ApplicationDbContext> options;
+
+        public StudyRoomBookingRepositoryTests()
+        {
+            studyRoomBooking_One = new StudyRoomBooking()
+            {
+                FirstName = "Ben1",
+                LastName = "Spark1",
+                Date = new DateTime(2023, 1, 1),
+                BookingId = 11,
+                StudyRoomId = 1,
+                Email = "1@wp.pl"
+            };
+
+            studyRoomBooking_Two = new StudyRoomBooking()
+            {
+                FirstName = "Ben2",
+                LastName = "Spark2",
+                Date = new DateTime(2023, 2, 2),
+                BookingId = 22,
+                StudyRoomId = 2,
+                Email = "2@wp.pl"
+            };
+        }
+        [SetUp]
+        public void Setup()
+        {
+            options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "temp_Bongo").Options;
+        }
+
+        [Test]
+        [Order(1)]
+        public void SaveBooking_BookingOne_CheckTheValuesFromDatabase()
+        {
+            //arrange
+
+
+            //act
+            using (var context = new ApplicationDbContext(options))
+            {
+                var repository = new StudyRoomBookingRepository(context);
+                repository.Book(studyRoomBooking_One);
+            }
+
+            //assert
+            using (var context = new ApplicationDbContext(options))
+            {
+                var bookingFromDb = context.StudyRoomBookings.FirstOrDefault(u => u.BookingId == 11);
+                Assert.AreEqual(studyRoomBooking_One.BookingId, bookingFromDb.BookingId);
+                Assert.AreEqual(studyRoomBooking_One.FirstName, bookingFromDb.FirstName);
+                Assert.AreEqual(studyRoomBooking_One.LastName, bookingFromDb.LastName);
+                Assert.AreEqual(studyRoomBooking_One.Email, bookingFromDb.Email);
+                Assert.AreEqual(studyRoomBooking_One.Date, bookingFromDb.Date);
+            }
+        }
+
+        [Test]
+        [Order(2)]
+        public void GetALlBooking_BookingOneAndTwo_CheckBothTheBookingsFromDabase()
+        {
+            //arrange
+            var expectedResult = new List<StudyRoomBooking> { studyRoomBooking_One, studyRoomBooking_Two };
+
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Database.EnsureDeleted();
+                var repository = new StudyRoomBookingRepository(context);
+                repository.Book(studyRoomBooking_One);
+                repository.Book(studyRoomBooking_Two);
+            }
+
+            //act
+            List<StudyRoomBooking> actualList;
+            using (var context = new ApplicationDbContext(options))
+            {
+                var repository = new StudyRoomBookingRepository(context);
+                actualList = repository.GetAll(null).ToList();
+            }
+
+            //assert
+            CollectionAssert.AreEqual(expectedResult, actualList, new BookingCompare());
+        }
+    }
+
+    public class BookingCompare : IComparer
+    {
+        public int Compare(object? x, object? y)
+        {
+            var booking1 = (StudyRoomBooking)x;
+            var booking2 = (StudyRoomBooking)y;
+            if (booking1.BookingId != booking2.BookingId)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
