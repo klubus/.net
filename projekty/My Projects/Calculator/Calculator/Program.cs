5e443c1dc3871c93ef54bf4562a4380b7a1cefb9
Krystian Kluba <klubakrystian@gmail.com>
2023-12-24 16:16:39 +0100

    Dice game

---
+++ projekty/My Projects/Calculator/Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿int result;
+int firstNumber;
+int secondNumber;
+
+Console.WriteLine("Hello, welcome to the calculator program!");
+
+Console.WriteLine("Please enter your first number");
+firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter your second number");
+secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("What type of operation would you like to do?");
+Console.WriteLine("Please enter a for addition, s for subtraction, m for multiplication or any other key for division");
+var operationType = Console.ReadLine();
+
+
+if (operationType == "a")
+{
+    result = firstNumber + secondNumber;
+}
+else if (operationType == "s")
+{
+    result = firstNumber - secondNumber;
+}
+else if (operationType == "m")
+{
+    result = firstNumber * secondNumber;
+}
+else
+{
+    result = firstNumber / secondNumber;
+}
+
+Console.WriteLine("The result is: " + result);
+Console.WriteLine("Thank you dor using calculator program!");
---
+++ projekty/My Projects/Dice Game/Dice Game/Program.cs
@@ -0,0 +1,51 @@
+﻿Random r = new Random();
+int userRandom;
+int aiRandom;
+int userScore = 0;
+int aiScore = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Press any key to roll the dice.");
+    Console.ReadKey(true);
+
+    userRandom = r.Next(1, 7);
+    aiRandom = r.Next(1, 7);
+    Console.WriteLine("You rolled a: " + userRandom);
+    Console.WriteLine("...");
+    System.Threading.Thread.Sleep(1000);
+    Console.WriteLine("Enemy AI rolled a: " + aiRandom);
+
+    if (userRandom > aiRandom)
+    {
+        Console.WriteLine("Player wins this round");
+        userScore++;
+    }
+    else if (aiRandom > userRandom)
+    {
+        Console.WriteLine("Enemy wins this round");
+        aiScore++;
+    }
+    else
+    {
+        Console.WriteLine("Draw!");
+        userScore++;
+        aiScore++;
+    }
+    Console.WriteLine($"The score is now - Player: {userScore}. Enemy: {aiScore}");
+    Console.WriteLine("");
+    Console.WriteLine("");
+}
+
+if (userScore > aiScore)
+{
+    Console.WriteLine("You WIN!");
+}
+else if (aiScore > userScore)
+{
+    Console.WriteLine("Enemy WIN");
+}
+else
+{
+    Console.WriteLine("DRAW");
+}
