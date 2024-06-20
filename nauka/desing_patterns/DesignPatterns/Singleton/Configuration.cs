d47825fc66532228d830a2baa6882a78c19422ee
k.kluba <k.kluba@mccomp.pl>
2024-06-20 17:59:10 +0200

    Added Singleton

---
+++ nauka/desing_patterns/DesignPatterns/Singleton/Configuration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Configuration
+    {
+        private static Configuration _instance = null;
+        public string StringProperty;
+        public int IntProperty;
+        private static object obj = new object();
+
+        private Configuration()
+        {
+        }
+
+        public static Configuration GetInstance()
+        {
+            // lock helps working on multiple threads
+            lock (obj)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Configuration();
+                }
+            }
+
+            return _instance;
+        }
+
+    };
+}
---
+++ nauka/desing_patterns/DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Configuration.GetInstance();
+            var b = Configuration.GetInstance();
+
+            if (ReferenceEquals(a,b))
+            {
+                Console.WriteLine("Configuration is singleton.");
+            }
+
+        }
+    }
+}
