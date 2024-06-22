e6d9560871d4f1a82bc0b9b9f5c23cd0f5cccb03
k.kluba <k.kluba@mccomp.pl>
2024-06-22 21:06:47 +0200

    Factory

---
+++ nauka/desing_patterns/DesignPatterns/Factory/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapeFactory = new ShapeFactory();
+            var circle = shapeFactory.CreateShape(ShapeType.Circle);
+
+            circle.Render();
+
+            // Wzorzec fabryka oddziela kod, który tworzy konkretne obiekty od kodu, który faktycznie z nich korzysta
+            // Dlatego w ten sposób jest dużo łatwiej rozszerzać kod tworzący nowe obiekty, bez konieczności ingerencji w resztę kodu.
+        }
+    }
+}
